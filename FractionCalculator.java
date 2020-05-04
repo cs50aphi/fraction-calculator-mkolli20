011907bb3b533bf93ce53fca5e7d739e1584fe01
Mihir Kolli <mkolli20@punahou.edu>
2020-05-04 19:32:11 +0000

    Introduction complete

---
+++ FractionCalculator.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class FractionCalculator
+{
+    public static void main(String[] args)
+    {
+        Scanner kb = new Scanner(System.in);
+        Fraction one;
+        Fraction two;
+        Fraction ans;
+        String operation;
+        boolean quit = false;
+
+        System.out.println("Welcome to the Fraction Calculator!");
+        System.out.println("It will add, subtract, multiply, and divide fractions until you type Q to quit.");
+        System.out.println("Please enter your fractions in the form a/b where a and b are both integers.");
+
+
+        while(!quit)
+        {
+            
+        }
+
+
+
+    }
+}
+
+public static String getOperation(Scanner kb)
+{
+    
+    String validOperations
+    
+    validOperations.indexOf('')
+}
